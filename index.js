4ebede7a919b82a90f4dbab2c511954ac4d13107
rcardin <rcardin@seidor.com>
2025-06-01 02:36:58 +0200

    wip

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { compressString, decompressString } from './compressor.js'
+import cors from 'cors'
 
 const app = express()
 // app.use(express.json())
+
+app.use(cors())
 app.use(express.text())
 const PORT = process.env.PORT || 3000
 
